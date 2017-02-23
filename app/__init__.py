4f6aea09acc39c177ecbe8b7b4ac0c169b6f17e0
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2017-02-23 15:59:46 -0500

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,35 @@
+'''
+Assemble our service.
+'''
+from flask import Flask
+import logging
+
+_logger = logging.getLogger(__name__)
+
+def _initialize_blueprints(application):
+    '''
+    Register Flask blueprints
+    '''
+    from app.views.sumologic import sumologic
+    application.register_blueprint(sumologic, url_prefix='/api/v1/sumologic')
+
+def _initialize_errorhandlers(application):
+    '''
+    Initialize error handlers
+    '''
+    from app.errors import errors
+    application.register_blueprint(errors)
+
+def create_app():
+    '''
+    Create an app by initializing components.
+    '''
+    _logger.info('Initializing application')
+    application = Flask(__name__)
+
+    _initialize_errorhandlers(application)
+    _initialize_blueprints(application)
+
+    # Do it!
+    return application
+
